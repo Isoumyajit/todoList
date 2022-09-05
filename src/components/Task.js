75b4afca4998a3f53e7668ad32f11e344124ae9b
Soumayjit Chakraborty <soumyo.chak1999@gmail.com>
2022-09-05 13:47:44 +0530

    Cards Updated css Fixed a little

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,35 +1,42 @@
-import React from "react";
-import { FaPenSquare } from "react-icons/fa";
-import { MdDeleteForever } from "react-icons/md";
+import React from 'react'
+import { FaPenSquare } from 'react-icons/fa'
+import { MdDeleteForever } from 'react-icons/md'
 
 const Task = (props) => {
   const handleDelete = () => {
-    props.deleteTask(props.indexNo);
-  };
+    props.deleteTask(props.indexNo)
+  }
   return (
     <>
       <div className="note mr-5">
         <div
-          class="card-top"
+          className="card-top"
           style={{
-            "background-color": props.colors[props.indexNo % 5].primaryColor,
+            'background-color': props.colors[props.indexNo % 5].primaryColor,
           }}
         ></div>
         <div className="task-heading">
           <span
-            class="card-header"
+            className="card-header"
             style={{
-              "background-color":
+              'background-color':
                 props.colors[props.indexNo % 5].secondaryColor,
-              "border-radius": "5px",
+              'border-radius': '5px',
             }}
           >
             {props.taskObj.Name}
           </span>
         </div>
-        <p>{props.taskObj.Des}</p>
+        <div
+          className="description-note"
+          style={{
+            'background-color': props.colors[props.indexNo % 5].secondaryColor,
+          }}
+        >
+          <p>{props.taskObj.Des}</p>
+        </div>
         <div className="notes-footer">
-          <p>Time created {props.indexNo}</p>
+          {/* <p>Time created {props.indexNo}</p> */}
           <div className="functional-buttons">
             <FaPenSquare
               className="edit-button mr-3"
@@ -49,7 +56,7 @@ const Task = (props) => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Task;
+export default Task
