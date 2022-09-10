1869209c0e81ed63d0ad86efca31fa66e1a32c3c
Soumayjit Chakraborty <soumyo.chak1999@gmail.com>
2022-09-10 19:08:48 +0530

    Form validataion added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,34 +1,67 @@
-import React, { useState } from 'react'
-import Button from 'react-bootstrap/Button'
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
 
 const Form = (props) => {
-  const [taskHeading, setTaskTitle] = useState('')
-  const [taskDescription, setTaskDescription] = useState('')
+  const [taskHeading, setTaskTitle] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
+  const [error, setError] = useState(false);
+
+  // TODO : Trying to make a input object to make it simple playing with inputs
+  // const [values, setFormValues] = useState({
+  //   taskName: "",
+  //   description: "",
+  // });
+
+  // const inputs = [
+  //   {
+  //     id: 1,
+  //     name: "taskName",
+  //     type: "text",
+  //     placeholder: "Task Name",
+  //     label: "TaskName",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "taskDescription",
+  //     type: "text",
+  //     placeholder: "Describe the task",
+  //     label: "Task Description",
+  //   },
+  // ];
 
   // ** Here this Arrow funtion is used to handdle the change events for the TaskHeading field
   const handleChangeTask = (event) => {
-    const taskName = event.target.value
-    setTaskTitle(taskName)
-  }
+    const taskName = event.target.value;
+    setTaskTitle(taskName);
+  };
 
   // ** Here this Arrow funtion is used to handdle the change events for the TaskDescrption Field
   const handleChangeTaskDescription = (event) => {
-    const descr = event.target.value
-    setTaskDescription(descr)
-  }
+    const descr = event.target.value;
+    setTaskDescription(descr);
+  };
 
-  const handleSave = () => {
-    let taskList = {}
-    taskList['Name'] = taskHeading
-    taskList['Des'] = taskDescription
-    props.saveTheTask(taskList)
-    props.toggle()
-  }
+  const validateData = () => {
+    if (taskHeading.length === 0 || taskDescription.length === 0) {
+      setError(true);
+    } else return true;
+  };
+  const handleSave = (event) => {
+    event.preventDefault();
+
+    if (validateData()) {
+      let taskList = {};
+      taskList["Name"] = taskHeading;
+      taskList["Des"] = taskDescription;
+      props.saveTheTask(taskList);
+      props.toggle();
+    }
+  };
 
   return (
     <>
       <div>
-        <form>
+        <form onSubmit={handleSave}>
           <div class="mb-3">
             <label for="Title" class="form-label">
               Title
@@ -41,28 +74,45 @@ const Form = (props) => {
               value={taskHeading}
               onChange={handleChangeTask}
             />
+            {error && taskHeading.length === 0 ? (
+              <div className="formErrors">
+                <label>Task Title cann't be blank </label>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
           <div class="mb-3">
             <label for="Description" class="form-label">
               Description
             </label>
             <textarea
-              placeholder="Describe the task"
+              placeholder="Describe The Task"
               class="form-control"
               id="Description"
               value={taskDescription}
               onChange={handleChangeTaskDescription}
             />
+            {error && taskDescription.length <= 15 ? (
+              <div className="formErrors">
+                <label>
+                  Task Description cann't be less than 15 characters{" "}
+                </label>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
+
           <div className="form-buttons">
-            <Button class="btn btn-primary bt-2" onClick={handleSave}>
+            <Button type="submit" class="btn btn-primary bt-2">
               Create
             </Button>
           </div>
         </form>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
