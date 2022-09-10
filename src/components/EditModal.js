37f93e59bebf523fd7bfb043fe86354d5e9889cc
Soumayjit Chakraborty <soumyo.chak1999@gmail.com>
2022-09-10 19:20:00 +0530

    Form validation added

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -1,38 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import Button from 'react-bootstrap/Button'
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
 
 function EditModal(props) {
-  const [taskHeading, setTaskTitle] = useState('')
-  const [taskDescription, setTaskDescription] = useState('')
+  const [taskHeading, setTaskTitle] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
+  const [errors, setErrors] = useState(false);
 
-  const editEventHandler = () => {
-    let newTask = {}
-    newTask['taskName'] = taskHeading
-    newTask['taskDescription'] = taskDescription
-    props.editTask(props.taskObj._id, newTask)
-    props.toggle()
-  }
+  const validateForm = () => {
+    if (taskHeading.length === 0 || taskDescription.length === 0) {
+      setErrors(true);
+    } else return true;
+  };
+  const editEventHandler = (event) => {
+    event.preventDefault();
+    if (validateForm()) {
+      let newTask = {};
+      newTask["taskName"] = taskHeading;
+      newTask["taskDescription"] = taskDescription;
+      props.editTask(props.taskObj._id, newTask);
+      props.toggle();
+    }
+  };
   // ** Here this Arrow funtion is used to handdle the change events for the TaskHeading field
   const handleChangeTask = (event) => {
-    const taskName = event.target.value
-    setTaskTitle(taskName)
-  }
+    const taskName = event.target.value;
+    setTaskTitle(taskName);
+  };
 
   // ** Here this Arrow funtion is used to handdle the change events for the TaskDescrption Field
   const handleChangeTaskDescription = (event) => {
-    const descr = event.target.value
-    setTaskDescription(descr)
-  }
+    const descr = event.target.value;
+    setTaskDescription(descr);
+  };
 
   useEffect(() => {
-    setTaskTitle(props.taskObj.taskName)
-    setTaskDescription(props.taskObj.taskDescription)
-  }, [props.taskObj.taskDescription, props.taskObj.taskName])
+    setTaskTitle(props.taskObj.taskName);
+    setTaskDescription(props.taskObj.taskDescription);
+  }, [props.taskObj.taskDescription, props.taskObj.taskName]);
 
   return (
     <>
       <div>
-        <form>
+        <form onSubmit={editEventHandler}>
           <div class="mb-3">
             <label for="Title" class="form-label">
               Title
@@ -45,6 +54,13 @@ function EditModal(props) {
               value={taskHeading}
               onChange={handleChangeTask}
             />
+            {errors && taskHeading.length === 0 ? (
+              <div className="formErrors">
+                <label>Task Title cann't be blank </label>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
           <div class="mb-3">
             <label for="Description" class="form-label">
@@ -57,16 +73,25 @@ function EditModal(props) {
               value={taskDescription}
               onChange={handleChangeTaskDescription}
             />
+            {errors && taskDescription.length <= 15 ? (
+              <div className="formErrors">
+                <label>
+                  Task Description cann't be less than 15 characters{" "}
+                </label>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
           <div className="form-buttons">
-            <Button class="btn btn-primary bt-2" onClick={editEventHandler}>
+            <Button type="submit" class="btn btn-primary bt-2">
               Update
             </Button>
           </div>
         </form>
       </div>
     </>
-  )
+  );
 }
 
-export default EditModal
+export default EditModal;
